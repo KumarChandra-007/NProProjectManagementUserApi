a9f0d091d9c47514769504c8a3b7528609a32095
abhiz1153 <53330510+abhiz1153@users.noreply.github.com>
2024-03-27 23:05:45 +0530

    Add files via upload

---
+++ NproProjectManagement.Common/Entities/Status.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NproProjectManagement.Common.Entities;
+
+public partial class Status
+{
+    public int StatusId { get; set; }
+
+    public string StatusName { get; set; } = null!;
+
+    public bool? IsActive { get; set; }
+
+    public DateTime CreatedOn { get; set; }
+
+    public int? CreatedBy { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public int? UpdatedBy { get; set; }
+}
