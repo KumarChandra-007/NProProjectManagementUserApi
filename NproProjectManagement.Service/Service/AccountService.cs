ae77cbd73ba8e35b967f410b58980d9733a0be61
rajkumarbapuvrs <rajkumarbapuvrs@gmail.com>
2024-04-03 13:48:15 +0530

    Code review

--- NproProjectManagement.Service/Service/AccountService.cs
+++ NproProjectManagement.Service/Service/AccountService.cs
@@ -1,16 +1,9 @@
 ﻿using AutoMapper;
 using Common.ViewModels;
-using Microsoft.Data.SqlClient;
 using NproProjectManagement.Common.Models;
 using NproProjectManagement.Common.ViewModels;
 using Repositories.Interface;
 using Services.Interface;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Services.Service
 {
@@ -57,16 +50,9 @@ namespace Services.Service
                 res.PhoneNumber = user.PhoneNumber;
                 res.EmployeeCode = user.EmployeeCode;
                 res.Region = user.Region;
-
-                //var res = _mapper.Map(user, viewModel);
-                //var result = _mapper.Map<UserViewModel>(user);
-
                 return res;
             }
-            else
-            {
-                return null;
-            }
+            return null;
         }
         public async Task<List<UserViewModel>> GetAllUserDetailsAsync()
         {
@@ -74,8 +60,6 @@ namespace Services.Service
             var user = await _accountRepository.GetAllUserAsync();
             if (user != null)
             {
-
-
                 foreach (var item in user)
                 {
                     var res = new UserViewModel();
@@ -90,51 +74,26 @@ namespace Services.Service
                     res.Region = item.Region;
                     result.Add(res);
                 }
-                // return _mapper.Map<List<UserViewModel>>(user);
                 return result;
             }
-            else
-            {
-                return null;
-            }
+            return null;
         }
 
         public async Task<AllProjectInfo> GetProjectUserTaskMappingAsync()
         {
-            var allProjectInfo = new AllProjectInfo();
-            var result = new List<ProjectUserTask>();
-            allProjectInfo = await _accountRepository.GetAllProjectInfo();
-
-            //var project = await _accountRepository.GetAllProjectsAsync();
-            //if (project != null)
-            //{
-            //    foreach (var item in project)
-            //    {
-            //        var res = new ProjectUserTask();
-
-            //        res.ProjectId = item.ProjectId;
-            //        res.Title = item.Title;
-            //        res.UserCount = await _accountRepository.GetUserCountByProjectId(item.ProjectId);
-            //        res.TaskCount = await _accountRepository.GetTaskCountByProjectId(item.ProjectId);
-            //        result.Add(res);
-            //    }
-
-            //    allProjectInfo.ProjectUserTaskGridInfo = result;
-
-                return allProjectInfo;
-            //}
-            //else
-            //{
-            //    return null;
-            //}
+            var allProjectInfo = await _accountRepository.GetAllProjectInfo();
+            return allProjectInfo;
         }
-        public async Task<int> SaveUser(User user){
+        public async Task<int> SaveUser(User user)
+        {
             return await _accountRepository.SaveUser(user);
         }
-        public async Task<int> UpdateUser(User user) {
+        public async Task<int> UpdateUser(User user)
+        {
             return await _accountRepository.UpdateUser(user);
         }
-        public async Task<int> DeleteUser(User user) {
+        public async Task<int> DeleteUser(User user)
+        {
             return await _accountRepository.DeleteUser(user);
         }
     }
